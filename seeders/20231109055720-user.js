401adfd04785389929b3a38e289fbb308b18c199
unknown <holidmuhamadsalman@gmail.com>
2023-11-12 02:18:16 +0700

    create tabel dan input data user-role

--- seeders/20231109055720-user.js
+++ seeders/20231109055720-user.js
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const addUsers = [
-      {
-        name: 'Raie Aswajjillah',
-        username: 'R41iee',
-        email: 'rai@gmail.com',
-        password: '123',
-        createdAt: new Date(),
-        updatedAt: new Date()
+      async up(queryInterface, Sequelize) {
+            const addUsers = [
+                  {
+                        name: "Raie Aswajjillah",
+                        username: "R41iee",
+                        email: "rai@gmail.com",
+                        password: "123",
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                  },
+                  {
+                        name: "Ikhsan",
+                        username: "death_vader32",
+                        email: "ikhsan@gmail.com",
+                        password: "123",
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                  },
+                  {
+                        name: "Iffat",
+                        username: "nbilIffat",
+                        email: "nabil@gmail.com",
+                        password: "123",
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                  },
+            ];
+            return queryInterface.bulkInsert("Users", addUsers, {});
       },
-      {
-        name: 'Ikhsan',
-        username: 'death_vader32',
-        email: 'ikhsan@gmail.com',
-        password: '123',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'Iffat',
-        username: 'nbilIffat',
-        email: 'nabil@gmail.com',
-        password: '123',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ]
-    return queryInterface.bulkInsert('Users', addUsers, {})
-  },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Users', null, {})
-  }
+      async down(queryInterface, Sequelize) {
+            return queryInterface.bulkDelete("Users", null, {});
+      },
 };
