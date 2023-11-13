944992bed26edec6be0323ccb4fdb5eb2ea0fe40
RaiA133 <rai010303@gmail.com@gmail.com>
2023-11-13 21:28:50 +0700

    improvement: restore table User_posts, sebelum terhapus akibat merge

---
+++ seeders/20231111185455-user_post.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const addUser_posts = [{
+      unique_id: 1,
+      post_img: "image.jpg",
+      post_title: "Pencari Jodoh",
+      post_desc: "Mencari pasangan wanita yang setia",
+      post_category: "Website",
+      post_tags: "Pariwisata",
+      post_deadline: "30 Hari",
+      post_pricing: "Rp. 2.000.000",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      unique_id: 1,
+      post_img: "image.jpg",
+      post_title: "Pengerjaan Web GIS",
+      post_desc: "bisa membuat seperti google maps",
+      post_category: "GIS",
+      post_tags: "Teknologi",
+      post_deadline: "14 Hari",
+      post_pricing: "Rp. 10.000.000",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+  ]
+  return queryInterface.bulkInsert('User_posts', addUser_posts, {})
+},
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('User_posts', null, {})
+  }
+};
