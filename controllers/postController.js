fc8a38aa30309e210ccb5e68ce130620f9ee8305
unknown <holidmuhamadsalman@gmail.com>
2023-11-14 11:44:32 +0700

    improvement: menambahkan fitur get post by tags dan memperbaiki typo pada env.example

--- controllers/postController.js
+++ controllers/postController.js
@@ -59,6 +59,41 @@ class postController {
         });
       });
   }
+
+  // halaman POST PEKERJAAN | GET all data user_posts by tags, from params url ( middlewares : JWT | login needed )
+  static getPostByTags(req, res, next) {
+    const { post_tags } = req.params;
+    User_post.findOne({
+      where: {
+        post_tags
+      }
+    })
+      .then(data => {
+        if (!data) {
+          return res.status(404).json({
+            status: 'Failed',
+            halaman: 'Post',
+            message: 'Data Post Tidak Ditemukan!'
+          });
+        }
+        else {
+          return res.status(200).json({
+            status: 'Success',
+            halaman: 'Post',
+            message: `Kategori Postingan Berdasarkan ${data.post_tags}`,
+            data
+          });
+        }
+      })
+      .catch(err => {
+        return res.status(500).json({
+          status: 'Failed',
+          halaman: 'post',
+          message: 'Something went wrong',
+          error: err
+        });
+      });
+  }
 }
 
 module.exports = postController
--- routes/post.js
+++ routes/post.js
@@ -4,5 +4,6 @@ const middlewares = require('../middlewares')
 
 route.get('/post', middlewares.verifyToken, postController.getPost)
 route.get('/post/category/:post_category', middlewares.verifyToken, postController.getPostByCategory)
+route.get('/post/tags/:post_tags', middlewares.verifyToken, postController.getPostByTags)
 
 module.exports = route
