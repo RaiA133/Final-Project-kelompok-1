13e9517d00cbea9770cb1099bd8e13f5ff902caf
RaiA133 <rai010303@gmail.com@gmail.com>
2023-11-15 13:24:16 +0700

    bugfix: error handler app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use('/api/v1/', routers)
-// app.use(errorHandler);
+app.use(errorHandler);
 app.use('/profile/picture', express.static(path.join(__dirname, 'assets/img/users')))
 
 if (process.env.NODE_ENV !== 'test') {
