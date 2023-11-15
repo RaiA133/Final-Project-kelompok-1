c4089e030866c64aae24613e229a910808451eb8
RaiA133 <rai010303@gmail.com@gmail.com>
2023-11-15 22:39:11 +0700

    bugifx: ganti link ke navigate

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -9,6 +9,7 @@ import LoginPage from './pages/LoginPage';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+  const navigate = useNavigate()
   let location = useLocation();
 
   return (
@@ -38,9 +39,9 @@ function App() {
           <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 min-h-full bg-base-200">
             {/* Sidebar content here */}
-            <li><a href='/'>Home</a></li>
-            <li><a href='/login'>Login</a></li>
-            <li><a href='/register'>Register</a></li>
+            <li><a onClick={() => navigate("/")}>Home</a></li>
+            <li><a onClick={() => navigate("/login")}>Login</a></li>
+            <li><a onClick={() => navigate("/register")}>Register</a></li>
           </ul>
         </div>
       </div>
