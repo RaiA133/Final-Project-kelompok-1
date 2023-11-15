1e9f1db53bf19fd32cc53741cccffc05ea3ce372
RaiA133 <rai010303@gmail.com@gmail.com>
2023-11-15 23:34:42 +0700

    improvement: Partner line fix

--- views/src/components/Partner.jsx
+++ views/src/components/Partner.jsx
@@ -4,22 +4,28 @@ import msib from '../assets/logo/msib.png';
 
 function Partner() {
   return (
-    <>
-      <hr className='mb-5 border-black' />
+    <div className='pb-5'>
+      <hr className='mb-5 border-black border' />
       <div className="flex flex-col w-full lg:flex-row mb-5">
+
         <div className="grid flex-grow h-40 card rounded-box place-items-center">
           <img src={logoRakamin} alt="Logo Rakamin" />
         </div>
-        <div className="divider lg:divider-horizontal"></div>
+
+        <div className="divider lg:divider-horizontal divider-neutral"></div>
+
         <div className="grid flex-grow h-40 card rounded-box place-items-center">
           <img src={kampusMerdeka} alt="Kampus Merdeka" />
         </div>
-        <div className="divider lg:divider-horizontal"></div>
+
+        <div className="divider lg:divider-horizontal divider-neutral"></div>
+
         <div className="grid flex-grow h-40 card rounded-box place-items-center">
           <img src={msib} alt="MSIB" />
         </div>
+
       </div>
-    </>
+    </div>
   )
 }
 
--- views/src/pages/HomePage.jsx
+++ views/src/pages/HomePage.jsx
@@ -46,7 +46,7 @@ function HomePage() {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Halaman Home</h1>
             <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button onClick={() => navigate('/post')} className="btn btn-primary">Post Now</button>
+            <button onClick={() => navigate('/post')} className="btn px-12 btn-primary">Post Now</button>
           </div>
         </div>
       </div>
@@ -83,7 +83,7 @@ function HomePage() {
         <div className="col-span-2 min-[640px]:text-xl min-[768px]:text-2xl min-[1024px]:text-3xl min-[1280px]:text-5xl font-bold">
           <div>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book for research. <br />
-            <button className='btn btn-lg max-[640px]:px-12  min-[640px]:px-16 min-[768px]:px-20 btn-primary mt-20'>Get Started</button>
+            <button className='btn lg:btn-lg max-[640px]:px-12  min-[640px]:px-16 min-[768px]:px-20 btn-primary mt-20'>Get Started</button>
           </div>
         </div>
 
