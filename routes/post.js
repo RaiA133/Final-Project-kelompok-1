b7894f5e3d77245aa3548a89510ca5c57277d760
RaiA133 <rai010303@gmail.com@gmail.com>
2023-11-19 12:53:50 +0700

    improvement: hapus comment yg tidak perlu

--- routes/post.js
+++ routes/post.js
@@ -28,19 +28,6 @@ route.post('/post/create', middlewares.verifyToken, upload.single('file'), (req,
   const fileName = req.file.filename;
   postController.createPostingan(req, res, next, fileName); // mengirim nama file yg sama ke userController.updateProfile
 });
-// route.post('/post/create', middlewares.verifyToken, upload.single('file'), (req, res, next) => {
-//   try {
-//     const fileName = req.file.filename;
-//     postController.createPostingan(req, res, next, fileName);
-//   } catch (error) {
-//     res.status(400).json({
-//       status: [400, 'Failed'],
-//       halaman: 'Post',
-//       message: error.message,
-//       error: error,
-//     });
-//   }
-// });
 route.put('/post/update/:id', middlewares.verifyToken, upload.single('file'), (req, res, next) => {
   const fileName = req.file.filename;
   postController.updatePostingan(req, res, next, fileName); // mengirim nama file yg sama ke userController.updateProfile
