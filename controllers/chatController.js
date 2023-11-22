309ebedba4114db706e63eae8654a9eb97e0b058
RaiA133 <rai010303@gmail.com@gmail.com>
2023-11-22 11:43:04 +0700

    improvement: checkpoint table

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,10 +1,11 @@
 const { Chat } = require("../models")
+const { Message } = require("../models")
 const { Op } = require('sequelize');
 const { v4: uuidv4 } = require('uuid');
 
 class chatController {
 
-  // /chat/find-or-create | buat obrolan chat jika yg di input di body tidak ada / ada salah satu yg tidak ada
+  //  /chat/find-or-create | buat obrolan chat jika yg di input di body tidak ada / ada salah satu yg tidak ada
   static createUserChat(req, res, next) {
     const { userone_unique_id, usertwo_unique_id } = req.body
     Chat.findOne({
@@ -46,10 +47,10 @@ class chatController {
       });
   }
 
-  // /chat/find-all | cari semua obrolan chat yang ada kitanya , untuk user box
+  //  /chat/find-all | cari semua obrolan chat yang ada kitanya , untuk user box
   static findAllUserChats(req, res, next) {
-    const { unique_id: chat_unique_id  } = req.userData
     // const { chat_unique_id } = req.params  // ubah route juga jika ingin pakai params
+    const { unique_id: chat_unique_id  } = req.userData
     Chat.findAll({
       where: {
         [Op.or]: {
@@ -86,7 +87,7 @@ class chatController {
       });
   }
 
-  // /chat/find/:userone_unique_id/:usertwo_unique_id | cari obrolan chat yg spesifik
+  //  /chat/find/:userone_unique_id/:usertwo_unique_id | cari obrolan chat yg spesifik
   static findUserChat(req, res, next) {
     const { userone_unique_id, usertwo_unique_id } = req.params
     Chat.findOne({
@@ -120,6 +121,62 @@ class chatController {
           error: err
         });
       });
+  }
+
+
+  //  /messages | kirim pesan
+  static async createMessage(req, res, next) {
+    try {
+      const { chat_unique_id, text } = req.body
+      const { unique_id: sender_unique_id  } = req.userData
+      console.log(sender_unique_id)
+      const newMessage = await Message.create({
+        chat_unique_id,
+        sender_unique_id,
+        text,
+      })
+      res.status(201).json({
+        status: [201, 'Success'],
+        halaman: 'createMessage',
+        message: 'Kirim Message Berhasil!',
+        data: newMessage
+      });
+    }
+    catch (err) {
+      return res.status(500).json({
+        status: [500, 'Failed'],
+        halaman: 'createMessage',
+        message: 'Something went wrong',
+        error: err.message
+      });
+    }
+  }
+
+  //  /messages/fidn-all | get semua pesan
+  static async getMessage(req, res, next) {
+    try {
+      const { chat_unique_id } = req.params
+      // const { unique_id: chat_unique_id  } = req.userData
+      const response = await Message.findAll({
+        where: {
+          chat_unique_id,
+        }
+      })
+      res.status(200).json({
+        status: [200, 'Success'],
+        halaman: 'getMessage',
+        message: 'Get Message Berhasil!',
+        data: response
+      })
+    }
+    catch (err) {
+      return res.status(500).json({
+        status: [500, 'Failed'],
+        halaman: 'createMessage',
+        message: 'Something went wrong',
+        error: err
+      })
+    }
   }
 
 }
