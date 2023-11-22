148de60e9abb62236b0ebebe0c72bb36d9d21e10
unknown <holidmuhamadsalman@gmail.com>
2023-11-22 17:33:10 +0700

    fixbug: main.jsx

--- views/src/main.jsx
+++ views/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { PostContextProvider } from "./contexts/postContext.jsx";
+import { PostContextProvider } from "../contexts/postContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
       <React.StrictMode>
