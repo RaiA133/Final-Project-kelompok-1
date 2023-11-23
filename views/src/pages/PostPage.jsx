6773438fdf37c38dc2aa8f8b52d56de7e4c4d192
unknown <holidmuhamadsalman@gmail.com>
2023-11-23 22:23:38 +0700

    improvement: menambahkan pagination pada halaman postingan

--- views/src/pages/PostPage.jsx
+++ views/src/pages/PostPage.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import { PostContext } from "../contexts/postcontext";
 import { UserContext } from "../contexts/UserContext";
 
@@ -9,16 +8,20 @@ function PostPage() {
   const { postState } = useContext(PostContext);
   console.log(postState);
   const { userState, img_profile_link, set_img_profile_link } = useContext(UserContext);
-  // State untuk menyimpan kategori yang dipilih
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedTags, setSelectedTags] = useState(null);
 
-  // Fungsi untuk memfilter data berdasarkan kategori yang dipilih
   const filterByCategory = (category) => {
     setSelectedCategory(category);
+    setCurrentPage(1);
   };
+
   const filterByTags = (category) => {
     setSelectedTags(category);
+    setCurrentPage(1);
   };
 
   useEffect(() => {
@@ -30,6 +33,13 @@ function PostPage() {
       set_img_profile_link(link);
     }
   }, [userState]);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = postState
+    .filter((post) => selectedCategory === null || post.post_category === selectedCategory)
+    .filter((post) => selectedTags === null || post.post_tags === selectedTags)
+    .slice(indexOfFirstItem, indexOfLastItem);
 
   return (
     <div className="flex-auto grid-cols gap-4 mx-5 mt-5">
@@ -62,37 +72,38 @@ function PostPage() {
 
       <div className="col-span-3">
         <div className="flex-auto grid grid-cols gap-4 mt-5">
-          {postState
-            .filter((post) => selectedCategory === null || post.post_category === selectedCategory) // Filter berdasarkan kategori yang dipilih
-            .filter((post) => selectedTags === null || post.post_tags === selectedTags)
-            .map((post, id) => (
-              <div key={id} className="bg-white p-4 mb-4 rounded-lg shadow-md">
-                <h2 className="text-xl font-bold mb-2">{post.post_title}</h2>
-
-                <div className="flex items-center mb-2">
-                  <img src={img_profile_link || "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} alt="gambar projek" className="w-16 h-16 object-cover rounded-full" />
-                  <p>{userState.username || "username"}</p>
-                </div>
-
-                <p>{post.post_desc}</p>
-
-                <div className="mt-4">
-                  <span>Budget :</span>
-                  {post.max_price}
-                  <p>Project Status: On Going</p>
-                  <p>Worktime: {post.post_worktime}</p>
-                </div>
-
-                <div className="mt-4">
-                  <button className="btn btn-primary mr-2">Chat Owner</button>
-                  <button className="btn btn-primary mr-2">Ambil Pekerjaan</button>
-                  <button onClick={() => navigate(`/post/${post.id}`)} className="btn btn-primary">
-                    View
-                  </button>
-                </div>
+          {currentItems.map((post, id) => (
+            <div key={id} className="bg-white p-4 mb-4 rounded-lg shadow-md">
+              <h2 className="text-xl font-bold mb-2">{post.post_title}</h2>
+              <div className="flex items-center mb-2">
+                <img src={img_profile_link || "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} alt="gambar projek" className="w-16 h-16 object-cover rounded-full" />
+                <p>{userState.username || "username"}</p>
               </div>
-            ))}
+              <p>{post.post_desc}</p>
+              <div className="mt-4">
+                <span>Budget :</span>
+                {post.max_price}
+                <p>Project Status: On Going</p>
+                <p>Worktime: {post.post_worktime}</p>
+              </div>
+              <div className="mt-4">
+                <button className="btn btn-primary mr-2">Chat Owner</button>
+                <button className="btn btn-primary mr-2">Ambil Pekerjaan</button>
+                <button onClick={() => navigate(`/post/${post.id}`)} className="btn btn-primary">
+                  View
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
+      <div className="join grid grid-cols-2">
+        <button className="join-item btn btn-outline" onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+          Previous page
+        </button>
+        <button className="join-item btn btn-outline" onClick={() => setCurrentPage(currentPage + 1)} disabled={currentItems.length < itemsPerPage}>
+          Next
+        </button>
       </div>
     </div>
   );
