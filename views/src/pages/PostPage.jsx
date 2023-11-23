9bb15586c0455dfae427ba2965f1d402d09ad804
unknown <holidmuhamadsalman@gmail.com>
2023-11-23 21:00:58 +0700

    improvement: get postingan by kategori

--- views/src/pages/PostPage.jsx
+++ views/src/pages/PostPage.jsx
@@ -5,69 +5,80 @@ import { PostContext } from "../contexts/postcontext";
 import { UserContext } from "../contexts/UserContext";
 
 function PostPage() {
-      const navigate = useNavigate();
-      const { postState } = useContext(PostContext);
-      console.log(postState);
-      const { userState, img_profile_link, set_img_profile_link } = useContext(UserContext);
+  const navigate = useNavigate();
+  const { postState } = useContext(PostContext);
+  console.log(postState);
+  const { userState, img_profile_link, set_img_profile_link } = useContext(UserContext);
+  // State untuk menyimpan kategori yang dipilih
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-      useEffect(() => {
-            if (userState.img_profile) {
-                  const link = `${import.meta.env.VITE_BACKEND_BASEURL}/profile/picture/` + userState.img_profile;
-                  set_img_profile_link(link);
-            } else {
-                  const link = import.meta.env.VITE_PROFILE_DEFAULT;
-                  set_img_profile_link(link);
-            }
-      }, [userState]);
+  // Fungsi untuk memfilter data berdasarkan kategori yang dipilih
+  const filterByCategory = (category) => {
+    setSelectedCategory(category);
+  };
 
-      return (
-            <div className="flex-auto grid-cols gap-4 mx-5 mt-5">
-                  <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-                        <h2 className="text-xl font-bold mb-4">Kategori</h2>
-                        <ul>
-                              <li className="cursor-pointer hover:underline text-blue-500" onClick={() => navigate("/kategori1")}></li>
-                              <li className="cursor-pointer hover:underline text-blue-500" onClick={() => navigate("/kategori2")}>
-                                    Kategori 2
-                              </li>
-                              <li className="cursor-pointer hover:underline text-blue-500" onClick={() => navigate("/kategori3")}>
-                                    Kategori 3
-                              </li>
-                        </ul>
-                  </div>
-                  <div className="col-span-3">
-                        <div className="flex-auto grid grid-cols gap-4 mt-5">
-                              {postState.length > 0 &&
-                                    postState.map((post, id) => (
-                                          <div key={id} className="bg-white p-4 mb-4 rounded-lg shadow-md">
-                                                <h2 className="text-xl font-bold mb-2">{post.post_title}</h2>
+  useEffect(() => {
+    if (userState.img_profile) {
+      const link = `${import.meta.env.VITE_BACKEND_BASEURL}/profile/picture/` + userState.img_profile;
+      set_img_profile_link(link);
+    } else {
+      const link = import.meta.env.VITE_PROFILE_DEFAULT;
+      set_img_profile_link(link);
+    }
+  }, [userState]);
 
-                                                <div className="flex items-center mb-2">
-                                                      <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="gambar projek" className="w-16 h-16 object-cover rounded-full" />
-                                                      <p>{userState.username || "username"}</p>
-                                                </div>
+  return (
+    <div className="flex-auto grid-cols gap-4 mx-5 mt-5">
+      <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+        <h2 className="text-xl font-bold mb-4">Kategori</h2>
+        <ul>
+          <li className="cursor-pointer hover:underline text-blue-500" onClick={() => filterByCategory(null)}>
+            All
+          </li>
+          {postState.length > 0 &&
+            [...new Set(postState.map((post) => post.post_category))].map((category, id) => (
+              <li key={id} className="cursor-pointer hover:underline text-blue-500" onClick={() => filterByCategory(category)}>
+                {category}
+              </li>
+            ))}
+        </ul>
+      </div>
 
-                                                <p>{post.post_desc}</p>
+      <div className="col-span-3">
+        <div className="flex-auto grid grid-cols gap-4 mt-5">
+          {postState
+            .filter((post) => selectedCategory === null || post.post_category === selectedCategory) // Filter berdasarkan kategori yang dipilih
+            .map((post, id) => (
+              <div key={id} className="bg-white p-4 mb-4 rounded-lg shadow-md">
+                <h2 className="text-xl font-bold mb-2">{post.post_title}</h2>
 
-                                                <div className="mt-4">
-                                                      <span>Budget :</span>
-                                                      {post.max_price}
-                                                      <p>Project Status: On Going</p>
-                                                      <p>Worktime: {post.post_worktime}</p>
-                                                </div>
+                <div className="flex items-center mb-2">
+                  <img src={img_profile_link || "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} alt="gambar projek" className="w-16 h-16 object-cover rounded-full" />
+                  <p>{userState.username || "username"}</p>
+                </div>
 
-                                                <div className="mt-4">
-                                                      <button className="btn btn-primary mr-2">Chat Owner</button>
-                                                      <button className="btn btn-primary mr-2">Ambil Pekerjaan</button>
-                                                      <button onClick={() => navigate(`/post/${post.id}`)} className="btn btn-primary">
-                                                            View
-                                                      </button>
-                                                </div>
-                                          </div>
-                                    ))}
-                        </div>
-                  </div>
-            </div>
-      );
+                <p>{post.post_desc}</p>
+
+                <div className="mt-4">
+                  <span>Budget :</span>
+                  {post.max_price}
+                  <p>Project Status: On Going</p>
+                  <p>Worktime: {post.post_worktime}</p>
+                </div>
+
+                <div className="mt-4">
+                  <button className="btn btn-primary mr-2">Chat Owner</button>
+                  <button className="btn btn-primary mr-2">Ambil Pekerjaan</button>
+                  <button onClick={() => navigate(`/post/${post.id}`)} className="btn btn-primary">
+                    View
+                  </button>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PostPage;
