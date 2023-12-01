78f767107c1847c092d539de57bede267e0d13b8
RaiA133 <rai010303@gmail.com@gmail.com>
2023-12-02 00:37:14 +0700

    feature: notif pesan sekarang ada keterangannya juga seperti notif friend req

--- views/src/components/Notification.jsx
+++ views/src/components/Notification.jsx
@@ -1,11 +1,13 @@
-import { useContext } from "react";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom"
 import BellAlert from '../assets/icon/bell-alert.svg'
 import { getUserByUniqueIdChat } from "../modules/fetch"
 import { ChatContext } from "../contexts/ChatContext";
 import { unreadNotificationsFunc } from "../hooks/unreadNotificationsFunc";
+import moment from "moment";
 
 const Notification = () => {
+  const navigate = useNavigate()
   const { user, userChats, notifications, allUsers, otherUserByUniqueId, setOtherUserByUniqueId, updateUserChat, setTextMessage } = useContext(ChatContext)
   const friendReqActive = userChats?.filter(item => item.friend_req !== null); // data percakapan/obrolan yg belum acc friend request
   const friendReqSender = friendReqActive && friendReqActive?.length > 0 ? friendReqActive : null 
@@ -43,7 +45,7 @@ const Notification = () => {
   const newArrayForUserChatUpdate = friendReqSender?.map((friendReq) => {
     const user = otherUserByUniqueId?.find((user) => user?.unique_id === friendReq?.friend_req);
     const receiverUserUniqueId = friendReq.members.filter(member => member !== friendReq.friend_req); // data unique_id user yg dikirim friend_req
-    const receiverUsername = allUsers.find(item => item?.unique_id === receiverUserUniqueId[0]); // data profile user yg dikirim friend_req
+    const receiverUsername = allUsers.find(user => user?.unique_id === receiverUserUniqueId[0]); // data profile user yg dikirim friend_req
     return { // memfilter data yg dibutuhkan saja untuk notifikasi dibawah
       chat_unique_id: friendReq?.chat_unique_id,
       friend_req: friendReq?.friend_req,
@@ -105,17 +107,18 @@ const Notification = () => {
                 </div>
               </div>
             </summary>
+            {/* notif acc friend req / friend req pending */}
             {newArrayForUserChatUpdate && newArrayForUserChatUpdate?.length > 0 && newArrayForUserChatUpdate[0] !== null ? (
               <ul className="p-2 bg-base-200 rounded-t-none rounded-box w-56 absolute right-0 z-10">
                 {newArrayForUserChatUpdate?.map(userNotif => (
                   <li key={userNotif?.chat_unique_id}>
                     {userNotif?.friend_req === user?.unique_id ? (
-                      <div className="flex">
+                      <div className="flex" title={moment(userNotif.date).calendar()}>
                         <a className="text-xs">Cencel friend request to <span className="font-bold">{userNotif?.to}</span></a>
                         <button className="btn btn-xs btn-neutral" onClick={() => handleOnUserChatUpdate(false, null, userNotif?.chat_unique_id)}>X</button>
                       </div>
                     ) : (
-                      <div className="flex">
+                      <div className="flex" title={moment(userNotif.date).calendar()}>
                         <a className="text-xs">You have a friend request from <span className="font-bold">{userNotif?.username}</span></a>
                         <button className="btn btn-xs btn-primary" onClick={() => handleOnUserChatUpdate(true, null, userNotif?.chat_unique_id)}>✓</button>
                         <button className="btn btn-xs btn-neutral" onClick={() => handleOnUserChatUpdate(false, null, userNotif?.chat_unique_id)}>X</button>
@@ -123,12 +126,17 @@ const Notification = () => {
                     )}
                   </li>
                 ))}
-                {/* <li>
-                  <div className="flex">
-                    <a className="text-xs">You have a message from <span className="font-bold">username</span></a>
-                    <button className="btn btn-xs btn-primary" onClick={() => handleOnUserChatUpdate(true, null, user?.chat_unique_id)}>See</button>
-                  </div>
-                </li> */}
+                {modifiedNotifications && modifiedNotifications.map((n, index) => (
+                  <li key={index}>
+                    <div className="flex">
+                      <a className="text-xs">You have a message from 
+                        <span className="font-bold"> {n.senderName}</span>
+                        <span className=""> {moment(n.date).calendar()}</span>
+                      </a>
+                      <button className="btn btn-xs btn-primary" onClick={() => navigate("/chat")}>See</button>
+                    </div>
+                  </li>
+                ))}
               </ul>
             ) : (
               <ul className="p-2 bg-base-200 rounded-t-none rounded-box w-56 absolute right-0 z-10">
